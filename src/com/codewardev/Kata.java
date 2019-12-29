6a7514cfa6ddc40e14c5eeaf90210201cb0551cc
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-29 20:12:03 +1100

    refined cut

--- src/com/codewardev/Kata.java
+++ src/com/codewardev/Kata.java
@@ -5,34 +5,14 @@ package com.codewardev;
 public class Kata {
 
 	public static char findMissingLetter(char[] array) {
-		char[] alphabets = {'a', 'b', 'c', 'd', 'e',
-		                    'f', 'g', 'h', 'i', 'j',
-		                    'k', 'l', 'm', 'n', 'o',
-		                    'p', 'q', 'r', 's', 't',
-		                    'u', 'v', 'w', 'x', 'y',
-		                    'z'};
-		boolean isLower = true;
-		if(Character.isUpperCase(array[0])) {
-			array = String.valueOf(array).toLowerCase().toCharArray();
-			isLower = false;
+
+		int thisChar = array[0];
+		for(int i=0; i<array.length; i++) {
+			if(thisChar != array[i]) return (char) thisChar;
+			thisChar++;
 		}
 		
-		char found = ' ';
-		int started=0;
-		for(int i=0; i<alphabets.length; i++) {
-			System.out.println("Compare: "+alphabets[i]+" and "+array[started]);
-			if(started == 0 && alphabets[i] == array[started]) {
-				started++;
-			} else
-			if(started != 0 && alphabets[i] != array[started]) {
-				found = alphabets[i];
-				break;
-			} else
-			if(started != 0 && alphabets[i] == array[started]){
-				started++;
-			}
-		}
-		return !isLower ? Character.toUpperCase(found): found;
+		return ' ';
 	}
 
 }
